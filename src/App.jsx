480603e29f8dbb7234b2d691ddc129d0e541d713
ErdemBilgn <mr.erdembilgin@gmail.com>
2025-08-18 17:07:45 +0300

    Created components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Portfolio from "./components/portfolio/Portfolio";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>App</div>
+      <About />
+      <Contact />
+      <Experience />
+      <Footer />
+      <Header />
+      <Nav />
+      <Portfolio />
     </>
   );
 }
